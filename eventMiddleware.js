7a5e04d2ac23b9fb9cea9233171bbc979e562b77
Kaleb Raymond <kalebraymond@Kalebs-MacBook-Air.local>
2023-03-27 16:10:17 -0400

    Renamed events.json to db.json

--- eventMiddleware.js
+++ eventMiddleware.js
@@ -1,6 +1,6 @@
 const low = require("lowdb");
 const FileSync = require("lowdb/adapters/FileSync");
-const adapter = new FileSync("events.json");
+const adapter = new FileSync("db.json");
 const db = low(adapter);
 const moment = require("moment");
 const crypto = require("crypto");
